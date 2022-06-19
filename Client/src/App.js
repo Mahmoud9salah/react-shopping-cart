7162b5f2d96782e19211638356f5a6ae8410e6ed
mahmoud9salah <mahmoudrttt7531@gmail.com>
2022-06-19 10:19:58 +0200

    component header

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,15 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      welcome 
+    <div className="layout">
+      <Header />
+      <main>
+        Content
+      </main>
+      <footer>
+        Footer
+      </footer>
     </div>
   );
 }
