8ef618f8a8eb574028f9acdc1f112a3064036e52
mahmoud9salah <mahmoudrttt753@gmail.com>
2022-06-22 11:09:26 +0200

    animation part 2

--- Client/src/components/Cart/Cart.js
+++ Client/src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import '../../css/Cart/Cart.css';
 import Checkout from '../CheckoutFrom/CheckoutFrom';
+import Bounce from 'react-reveal/Bounce'
 
 function Cart(props) {
 
@@ -27,24 +28,27 @@ function Cart(props) {
             <div className='cart-title'>{props.cartItems.length === 0 ? 'Cart Empty' : <p>
                 There is {props.cartItems.length} product in cart
                 </p>}</div>
-            <div className='cart-items'>
-                {props.cartItems.map(item => (
-                        <div className='cart-item' key={item.id}>
-                            <img src={item.imageUrl} alt='' />
-                            <div className='cart-info'>
-                                <div>
-                                    <p>title: {item.title}</p>
-                                    <p>qty: {item.qty} </p>
-                                    <p>price: ${item.price}</p>
+            <Bounce bottom cascade>
+                
+                <div className='cart-items'>
+                    {props.cartItems.map(item => (
+                            <div className='cart-item' key={item.id}>
+                                <img src={item.imageUrl} alt='' />
+                                <div className='cart-info'>
+                                    <div>
+                                        <p>title: {item.title}</p>
+                                        <p>qty: {item.qty} </p>
+                                        <p>price: ${item.price}</p>
+                                    </div>
+                                    <button onClick={() => props.removeFromCart(item)}>
+                                        Remove
+                                    </button>
                                 </div>
-                                <button onClick={() => props.removeFromCart(item)}>
-                                    Remove
-                                </button>
                             </div>
-                        </div>
-                ))
-                }
-            </div>
+                    ))
+                    }
+                </div>
+            </Bounce>
 
                 {/* The Sub Total  */}
             {
@@ -70,6 +74,8 @@ function Cart(props) {
             handleChange={handleChange}
             submitOrder={submitOrder}
             />
+                
+            
         </div>
     )
 }
